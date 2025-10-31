6f0c1538154e5e2bd94016a9dd9f676dc82a753f
irfan2202 <irfanapurnomo02@gmail.com>
2025-10-31 11:10:30 +0700

    navlinks

---
+++ resources/views/components/nav-links.blade.php
@@ -0,0 +1,15 @@
+<li class="nav-item">
+    <a class="nav-link fw-semibold" href="{{ url('/') }}">Beranda</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link fw-semibold" href="{{ url('/#about') }}">Tentang</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link fw-semibold" href="{{ url('/#packages') }}">Paket</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link fw-semibold" href="{{ url('/#testimonials') }}">Testimoni</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link fw-semibold" href="{{ url('/#contact') }}">Kontak</a>
+</li>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,22 +7,7 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link fw-semibold" href="../index.html">Beranda</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link fw-semibold" href="../index.html#about">Tentang</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link fw-semibold" href="../index.html#packages">Paket</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link fw-semibold" href="../index.html#testimonials">Testimoni</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link fw-semibold" href="../index.html#contact">Kontak</a>
-                </li>
-
+                @include('components.nav-links')
             </ul>
         </div>
     </div>
