d78ffe4ed6ba31db2aaab1172537521d2e32ef47
irfan2202 <irfanapurnomo02@gmail.com>
2025-11-04 16:15:56 +0700

    footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,87 @@
-<div>
-    footer
-</div>
+<footer class="footer bg-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <h4>Al-Madinah Travel</h4>
+                <small>Travel Haji & Umroh</small>
+                <p>
+                    Melayani perjalanan haji dan umroh dengan penuh amanah sejak 2008.
+                    Berpengalaman mengantarkan ribuan jamaah untuk menunaikan rukun
+                    Islam kelima.
+                </p>
+
+                <div>
+                    <a href="#" class="social-icon">
+                        <i class="bi bi-facebook"></i>
+                    </a>
+                    <a href="#" class="social-icon">
+                        <i class="bi bi-instagram"></i>
+                    </a>
+                    <a href="#" class="social-icon">
+                        <i class="bi bi-youtube"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-6">
+                <h5>Navigasi</h5>
+                @include('components.nav-links')
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <h5 class="fw-bold text-white mb-4">Layanan Kami</h5>
+                <ul class="list-unstyled">
+                    <li class="mb-2 text-light opacity-75">Umroh Reguler</li>
+                    <li class="mb-2 text-light opacity-75">Umroh VIP</li>
+                    <li class="mb-2 text-light opacity-75">Haji Reguler</li>
+                    <li class="mb-2 text-light opacity-75">Haji Plus</li>
+                    <li class="mb-2 text-light opacity-75">Umroh Keluarga</li>
+                    <li class="mb-2 text-light opacity-75">City Tour</li>
+                </ul>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <h5 class="fw-bold text-white mb-4">Kontak</h5>
+                <i class="bi bi-geo-alt-fill"></i>
+                <small>
+                    Jl. Raya Haji No. 123, Jakarta Selatan 12345
+                </small>
+                <i class="bi bi-telephone-fill"></i>
+                <a href="tel:+62085721122232">
+                    +62 085721122232
+                </a>
+                <i class="bi bi-envelope-fill"></i>
+                <a href="mailto:info@almadinahtravel.com">
+                    info@almadinahtravel.com
+                </a>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div>
+            <i class="bi bi-clock-fill"></i>
+            <div class="text-light opacity-75 small">
+                <div>Senin - Jumat: 08:00 - 20:00</div>
+                <div>Sabtu: 08:00 - 17:00</div>
+                <div>Minggu: 09:00 - 15:00</div>
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
+    <div class="row align-items-center">
+        <div class="col-md-6">
+            <p class="text-light opacity-75 mb-0 small">
+                © 2025 Al-Madinah Travel. Hak Cipta Dilindungi.
+            </p>
+        </div>
+        <div class="col-md-6 text-md-end">
+            <div class="d-flex flex-wrap gap-3 justify-content-md-end">
+                <a href="#">Syarat & Ketentuan</a>
+                <a href="#">Kebijakan Privasi</a>
+                <a href="#">FAQ</a>
+            </div>
+        </div>
+    </div>
+    </div>
+
+</footer>
